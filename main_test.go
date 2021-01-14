c6f20d35d88fef01eb3d6c0c1db5d0bd1aedd312
mehradsadeghi <2012.linkinpark@gmail.com>
2021-01-14 22:36:41 +0330

    Adding more tests.

--- main_test.go
+++ main_test.go
@@ -2,33 +2,111 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
 )
 
+const TestFileName = "test.txt"
+
 func Test_well(t *testing.T) {
-	fileName := "test.go"
-	content := []byte(`package main
+
+	t.Run("it can make the messy #1 well imported", func(t *testing.T) {
+		makeTempFileFilledWith(getMessySourceFileContent1())
+
+		well(TestFileName)
+
+		welledContent, err := ioutil.ReadFile(TestFileName)
+		if err != nil {
+			_ = fmt.Errorf("failed to load well imported file duo to %s", err.Error())
+			os.Exit(1)
+		}
+
+		t.Log(welledContent)
+		t.Log(getWellSourceFileContent1())
+
+		if bytes.Compare(welledContent, getWellSourceFileContent1()) != 0 {
+			t.Error("well importing didn't make the import portion well!")
+		}
+
+		tearDown()
+	})
+
+	t.Run("it can make the messy #2 well imported", func(t *testing.T) {
+		makeTempFileFilledWith(getMessySourceFileContent2())
+
+		well(TestFileName)
+
+		welledContent, err := ioutil.ReadFile(TestFileName)
+		if err != nil {
+			_ = fmt.Errorf("failed to load well imported file duo to %s", err.Error())
+			os.Exit(1)
+		}
+
+		if bytes.Compare(welledContent, getWellSourceFileContent2()) != 0 {
+			t.Error("well importing didn't make the import portion well!")
+		}
+
+		tearDown()
+	})
+}
+
+func tearDown() {
+	_ = os.Remove(TestFileName)
+}
+
+func makeTempFileFilledWith(content []byte) {
+	if err := ioutil.WriteFile(TestFileName, content, 0666); err != nil {
+		_ = fmt.Errorf("failed to create temp file duo to %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func getMessySourceFileContent1() []byte {
+	return []byte(`package main
 
 import (
     "fmt"
     "github.com/andybalholm/cascadia"
 )
 
-func sample() {
-    cascadia.Parse("A")
-    fmt.Println("A")
-}`)
-	ioutil.WriteFile(fileName, content, 0666)
+func someFunction() {}`)
+}
 
-	well(fileName)
+func getWellSourceFileContent1() []byte {
+	return []byte(`package main
 
-	welledContent, _ := ioutil.ReadFile(fileName)
+import (
+    "fmt"
 
-	if bytes.Compare(welledContent, content) != 0 {
-		t.Error("Didn't work!")
-	}
+    "github.com/andybalholm/cascadia"
+)
 
-	os.Remove(fileName)
+func someFunction() {}`)
+}
+
+func getMessySourceFileContent2() []byte {
+	return []byte(`package main
+
+import (
+    "fmt"
+    "os/signal"
+    "github.com/andybalholm/cascadia"
+)
+
+func someFunction() {}`)
+}
+
+func getWellSourceFileContent2() []byte {
+	return []byte(`package main
+
+import (
+    "fmt"
+    "os/signal"
+
+    "github.com/andybalholm/cascadia"
+)
+
+func someFunction() {}`)
 }
